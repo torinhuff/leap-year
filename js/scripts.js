b2ae138dced7d3568f54c2adba0357552da75885
Student <example@example.com>
2018-10-01 18:42:05 -0700

    this will return a 'false' message for any input

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+var leapYear = function(year) {
+  return false;
+};
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+    event.preventDefault();
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
+  });
+});
